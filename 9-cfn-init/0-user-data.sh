a5137876bd4c9cad966f7a1c256812c2cb65c064
Max <max@apurata.com>
2019-07-13 12:24:30 -0500

    cloud formation course

---
+++ 9-cfn-init/0-user-data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+yum update -y
+amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+yum install -y httpd mariadb-server
+systemctl start httpd
+systemctl enable httpd
+systemctl is-enabled httpd
+chkconfig httpd on
+groupadd www
+usermod -a -G www ec2-user
+chown -R root:www /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} +
+find /var/www -type f -exec chmod 0664 {} +
+echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
